ed5bd2085d49182ad90b1569452962f68087401f
Cocodrulo <cocodrulojr@gmail.com>
2024-07-09 13:06:16 +0100

    refactor: Added new icons

--- src/assets/icons/IconsHandler.ts
+++ src/assets/icons/IconsHandler.ts
@@ -1,6 +1,13 @@
 import AstroIcon from './AstroIcon.astro'
 import CSSIcon from './CSSIcon.astro'
+import DiscordJSIcon from './DiscordJSIcon.astro'
+import HTMLIcon from './HTMLIcon.astro'
 import JavaScriptIcon from './JavaScriptIcon.astro'
+import LuaIcon from './LuaIcon.astro'
+import MariaDBIcon from './MariaDBIcon.astro'
+import MongoDBIcon from './MongoDBIcon.astro'
+import MySQLIcon from './MySQLIcon.astro'
+import NodeJSIcon from './NodeJSIcon.astro'
 import SnipcartIcon from './SnipcartIcon.astro'
 import TailwindCSSIcon from './TailwindCSSIcon.astro'
 import TypeScriptIcon from './TypeScriptIcon.astro'
@@ -12,4 +19,11 @@ export default {
     TypeScript: { icon: TypeScriptIcon },
     TailwindCSS: { icon: TailwindCSSIcon },
     Snipcart: { icon: SnipcartIcon },
+    Lua: { icon: LuaIcon },
+    HTML: { icon: HTMLIcon },
+    MariaDB: { icon: MariaDBIcon },
+    MySQL: { icon: MySQLIcon },
+    MongoDB: { icon: MongoDBIcon },
+    DiscordJS: { icon: DiscordJSIcon },
+    NodeJS: { icon: NodeJSIcon },
 }
