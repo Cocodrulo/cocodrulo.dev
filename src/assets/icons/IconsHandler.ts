bf1b87435d7e2e4d8a5c4e4af56443196325a35d
Cocodrulo <cocodrulojr@gmail.com>
2024-07-09 12:03:59 +0100

    chore: Add CSSIcon, IconsHandler, TailwindCSSIcon, AstroIcon, JavaScriptIcon, and SnipcartIcon

---
+++ src/assets/icons/IconsHandler.ts
@@ -0,0 +1,15 @@
+import AstroIcon from './AstroIcon.astro'
+import CSSIcon from './CSSIcon.astro'
+import JavaScriptIcon from './JavaScriptIcon.astro'
+import SnipcartIcon from './SnipcartIcon.astro'
+import TailwindCSSIcon from './TailwindCSSIcon.astro'
+import TypeScriptIcon from './TypeScriptIcon.astro'
+
+export default {
+    Astro: AstroIcon,
+    CSS: CSSIcon,
+    JavaScript: JavaScriptIcon,
+    TypeScript: TypeScriptIcon,
+    TailwindCSS: TailwindCSSIcon,
+    Snipcart: SnipcartIcon,
+}
