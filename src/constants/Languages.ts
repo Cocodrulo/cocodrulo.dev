134362cd04140ea876265cb71a4e878d42b70a0d
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2024-07-15 22:01:07 +0100

    refactor: Update language flags URLs in Languages constant and added images

--- src/constants/Languages.ts
+++ src/constants/Languages.ts
@@ -2,11 +2,11 @@ export default {
     ['/es/']: {
         prefix: 'es',
         name: 'Español',
-        flag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgh2widkbW8rRkRhWP_Ppd1OI8GRLyiyNVyg&s',
+        flag: '/images/flags/es.png',
     },
     ['/en/']: {
         prefix: 'en',
         name: 'English',
-        flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/2560px-Flag_of_the_United_Kingdom_%283-5%29.svg.png',
+        flag: '/images/flags/en.png',
     },
 } as { [key: string]: { prefix: string; name: string; flag: string } }
