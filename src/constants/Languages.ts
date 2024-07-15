d42d5080d8898eebdf7601cbe27269a00b9d47c6
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2024-07-15 21:58:28 +0100

    refactor: Add language selection dropdown to Languages constant

--- src/constants/Languages.ts
+++ src/constants/Languages.ts
@@ -2,9 +2,11 @@ export default {
     ['/es/']: {
         prefix: 'es',
         name: 'Español',
+        flag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgh2widkbW8rRkRhWP_Ppd1OI8GRLyiyNVyg&s',
     },
     ['/en/']: {
         prefix: 'en',
         name: 'English',
+        flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/2560px-Flag_of_the_United_Kingdom_%283-5%29.svg.png',
     },
-} as { [key: string]: { prefix: string; name: string } }
+} as { [key: string]: { prefix: string; name: string; flag: string } }
