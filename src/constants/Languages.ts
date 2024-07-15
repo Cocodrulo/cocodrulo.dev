8746887b0aeccfced7e8b627d7ec74e1c1fadf95
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2024-07-15 17:07:21 +0100

    refactor: Add Languages constant for internationalization support

---
+++ src/constants/Languages.ts
@@ -0,0 +1,10 @@
+export default {
+    ['/es/']: {
+        prefix: 'es',
+        name: 'Español',
+    },
+    ['/en/']: {
+        prefix: 'en',
+        name: 'English',
+    },
+} as { [key: string]: { prefix: string; name: string } }
