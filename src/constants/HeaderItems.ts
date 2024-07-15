29b4426be84170aa7fc341efa9657e181baba16a
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2024-07-15 17:07:18 +0100

    refactor: Add language selection dropdown to Header component

---
+++ src/constants/HeaderItems.ts
@@ -0,0 +1,4 @@
+export default {
+    ['es']: [{ label: 'Mi Trabajo', link: '#work', name: 'work' }],
+    ['en']: [{ label: 'My Work', link: '#work', name: 'work' }],
+} as { [key: string]: { label: string; link: string; name: string }[] }
