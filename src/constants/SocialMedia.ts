cd4156af5bbb1c4633ebae578aaf2cc16f6672d8
Cocodrulo <cocodrulojr@gmail.com>
2024-11-23 22:45:57 +0000

    feat: Add SocialMedia constants for GitHub, LinkedIn, Twitter, and Instagram

---
+++ src/constants/SocialMedia.ts
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: 'GitHub',
+        link: 'https://github.com/cocodrulo',
+        icon: 'Github',
+    },
+    {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/javier-aday-p%C3%A9rez-romero-b5636a291/',
+        icon: 'Linkedin',
+    },
+    {
+        name: 'Twitter',
+        link: 'https://x.com/cocodrulo_dev',
+        icon: 'Twitter',
+    },
+    {
+        name: 'Instagram',
+        link: 'https://www.instagram.com/cocodrulo.dev/',
+        icon: 'Instagram',
+    },
+]
