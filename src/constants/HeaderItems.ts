451b71cd6034f1def425b0b1c0ebdd0d4af51fb8
Cocodrulo <cocodrulojr@gmail.com>
2024-11-23 22:45:54 +0000

    feat: Add social media section to header items for English and Spanish

--- src/constants/HeaderItems.ts
+++ src/constants/HeaderItems.ts
@@ -2,9 +2,11 @@ export default {
     ['es']: [
         { label: 'Mi Trabajo', link: '#work', name: 'work' },
         { label: 'Mi Estudios', link: '#studies', name: 'studies' },
+        { label: 'Redes Sociales', link: '#socialmedia', name: 'socialmedia' },
     ],
     ['en']: [
         { label: 'My Work', link: '#work', name: 'work' },
         { label: 'My Studies', link: '#studies', name: 'studies' },
+        { label: 'Social Media', link: '#socialmedia', name: 'socialmedia' },
     ],
 } as { [key: string]: { label: string; link: string; name: string }[] }
