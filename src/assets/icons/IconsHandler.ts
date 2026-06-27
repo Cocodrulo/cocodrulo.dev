72f9788c2bdf6d0f1e338c65442d2d6e25b099f4
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2026-06-27 21:54:17 +0100

    Added Mail icon

--- src/assets/icons/IconsHandler.ts
+++ src/assets/icons/IconsHandler.ts
@@ -1,23 +1,24 @@
-import AstroIcon from "./AstroIcon.astro";
-import CSSIcon from "./CSSIcon.astro";
-import DiscordJSIcon from "./DiscordJSIcon.astro";
-import HTMLIcon from "./HTMLIcon.astro";
-import JavaScriptIcon from "./JavaScriptIcon.astro";
-import LuaIcon from "./LuaIcon.astro";
-import MariaDBIcon from "./MariaDBIcon.astro";
-import MongoDBIcon from "./MongoDBIcon.astro";
-import MySQLIcon from "./MySQLIcon.astro";
-import NodeJSIcon from "./NodeJSIcon.astro";
-import SnipcartIcon from "./SnipcartIcon.astro";
-import TailwindCSSIcon from "./TailwindCSSIcon.astro";
-import TypeScriptIcon from "./TypeScriptIcon.astro";
-import DropdownIcon from "./Dropdown.astro";
-import Instagram from "./Instagram.astro";
-import Linkedin from "./Linkedin.astro";
-import Twitter from "./Twitter.astro";
-import Github from "./Github.astro";
-import ArrowIcon from "./ArrowIcon.astro";
-import Credly from "./Credly.astro";
+import AstroIcon from './AstroIcon.astro'
+import CSSIcon from './CSSIcon.astro'
+import DiscordJSIcon from './DiscordJSIcon.astro'
+import HTMLIcon from './HTMLIcon.astro'
+import JavaScriptIcon from './JavaScriptIcon.astro'
+import LuaIcon from './LuaIcon.astro'
+import MariaDBIcon from './MariaDBIcon.astro'
+import MongoDBIcon from './MongoDBIcon.astro'
+import MySQLIcon from './MySQLIcon.astro'
+import NodeJSIcon from './NodeJSIcon.astro'
+import SnipcartIcon from './SnipcartIcon.astro'
+import TailwindCSSIcon from './TailwindCSSIcon.astro'
+import TypeScriptIcon from './TypeScriptIcon.astro'
+import DropdownIcon from './Dropdown.astro'
+import Instagram from './Instagram.astro'
+import Linkedin from './Linkedin.astro'
+import Twitter from './Twitter.astro'
+import Github from './Github.astro'
+import ArrowIcon from './ArrowIcon.astro'
+import Credly from './Credly.astro'
+import Mail from './Mail.astro'
 
 export default {
     Astro: AstroIcon,
@@ -40,4 +41,5 @@ export default {
     Github: Github,
     ArrowIcon: ArrowIcon,
     Credly: Credly,
-};
+    Mail: Mail,
+}
