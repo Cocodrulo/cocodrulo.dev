817fcaa211af8a4aeab30898ccde7485b28d9487
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2026-06-30 14:06:05 +0100

    Extracted types

--- emdash-env.d.ts
+++ emdash-env.d.ts
@@ -97,6 +97,24 @@ export interface Social {
   terms?: Record<string, TaxonomyTerm[]>;
 }
 
+export interface Timeline {
+  id: string;
+  slug: string | null;
+  status: string;
+  title: string;
+  place: string;
+  type: 'work' | 'education';
+  start_year: string;
+  end_year?: string;
+  current?: boolean;
+  description?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date | null;
+  bylines?: ContentBylineCredit[];
+  terms?: Record<string, TaxonomyTerm[]>;
+}
+
 declare module "emdash" {
   interface EmDashCollections {
     about: About;
@@ -105,5 +123,6 @@ declare module "emdash" {
     profile: Profile;
     projects: Project;
     socials: Social;
+    timeline: Timeline;
   }
 }
