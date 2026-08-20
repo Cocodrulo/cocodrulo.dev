25a1f3b5a134b25f4d2f04ff103e01a634cdf8a4
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2026-08-20 20:24:18 +0100

    added short-link

---
+++ src/pages/short-links/[slug].ts
@@ -0,0 +1,26 @@
+import { getEmDashEntry } from 'emdash'
+
+export async function GET({ params }: { params: { slug: string } }) {
+    const { slug } = params
+
+    const { entry: shortLink, error } = await getEmDashEntry('short-links', slug)
+
+    if (error) {
+        return new Response('Server Error', {
+            status: 500,
+        })
+    }
+
+    if (!shortLink) {
+        return new Response('Data not found', {
+            status: 404,
+        })
+    }
+
+    return new Response(null, {
+        status: 301,
+        headers: {
+            Location: shortLink.data.url as string,
+        },
+    })
+}
